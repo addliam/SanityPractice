1512e6adef7986d67cc80023bb8c68272fc7c01f
codeli4m <applecdos504@gmail.com>
2023-05-23 13:05:42 -0500

    setup basico de lista mascotas sanity nextjs

---
+++ frontend/pages/index.js
@@ -0,0 +1,56 @@
+import Head from "next/head";
+import { createClient } from "next-sanity";
+export default function IndexPage({ pets }) {
+  return (
+    <>
+      <Head>
+        <title>Mis mascotas | NextJS</title>
+      </Head>
+      <header>
+        <h1>Sanity + Next.js</h1>
+      </header>
+      <main>
+        <h2>Lista de mascotas</h2>
+        {pets.length > 0 && (
+          <ul>
+            {pets.map((pet) => (
+              <li key={pet._id}>{pet?.name}</li>
+            ))}
+          </ul>
+        )}
+        {!pets.length > 0 && <p>No pets to show</p>}
+        {/* {pets.length > 0 && (
+          <div>
+            <pre>{JSON.stringify(pets, null, 2)}</pre>
+          </div>
+        )} */}
+        {!pets.length > 0 && (
+          <div>
+            <div>¯\_(ツ)_/¯</div>
+            <p>
+              Your data will show up here when you've configured everything
+              correctly
+            </p>
+          </div>
+        )}
+      </main>
+    </>
+  );
+}
+
+const client = createClient({
+  projectId: "3qo9t5hb",
+  dataset: "production",
+  apiVersion: "2023-02-15",
+  useCdn: false,
+});
+
+export async function getStaticProps() {
+  const pets = await client.fetch(`*[_type == "pet"]`);
+
+  return {
+    props: {
+      pets,
+    },
+  };
+}
---
+++ silver-oyster/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '3qo9t5hb',
+    dataset: 'production'
+  }
+})
